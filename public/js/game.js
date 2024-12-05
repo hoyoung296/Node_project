b22f43b5c14d57eb75098c927564628dac1d67a1
hoyoung <skghdud024@naver.com>
2024-12-05 16:54:33 +0900

    미니게임완

--- public/js/game.js
+++ public/js/game.js
@@ -4,17 +4,17 @@ let ctx;
 canvas = document.createElement("canvas") //canvas를 만들어서 변수에 저장
 ctx = canvas.getContext("2d") //2d로 그림을 그려줌
 //캔버스 사이즈 지정
-canvas.width = 400;
-canvas.height = 700;
-document.section.appendChild(canvas); //section의 body에다가 자식으로 canvas 갇다 붙이기
+canvas.width = 730;
+canvas.height = 600;
+document.getElementById("game").appendChild(canvas); //section의 body에다가 자식으로 canvas 갇다 붙이기
 
 let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
 let gameOver = false //true이면 게임이 끝남, false이면 게임이 안끝남
 let score = 0
 
 // 우주선 좌표 > 우주선 죄표는 계속 바뀌기 때문에 따로 뺴줌
-let spaceshipX = 152
-let spaceshipY = 604
+let spaceshipX = 317
+let spaceshipY = 504
 
 // 총알 만들기
 //1. 스페이스바를 누르면 총알발사
@@ -60,7 +60,6 @@ function Bullet() { //총알을 만들기 위한 자료
 // 3. 1초마다 하나씩 적군이 나온다.
 // 4. 적군의 우주선이 바닥에 닿으면 게임 오버
 // 5. 적군과 총알이 만나면 우주선이 사라진다 점수 1점 획득
-
 // 외계인도 지속적으로 여러개가 필요하기 때문에 따로 함수로 생성
 let enemyList = []//외계인들을 저장하는 리스트
 function generateRandomValue(min, max) {
@@ -80,7 +79,8 @@ function Enemy() { //외계인을 만들기 위한 자료
 
         if (this.y >= canvas.height - 64) {
             gameOver = true
-            let msg = `<br><button onclick="result_form()">결과 확인</button>`
+            let msg = `<br><button onclick="result_form()" style='color:gold; margin-top:-300px; width:100x; height:100px;'>
+            결과 확인</button>`
             document.getElementById("main").insertAdjacentHTML("beforeend", msg)
             // console.log(gameOver)
         }
@@ -110,7 +110,9 @@ function setupKeyboardListener() {
 }
 
 function kDown(event) {
-    if (spaceshipY >= 0 && spaceshipY <= 604 && spaceshipX >= 0 && spaceshipX <= 304) {
+    event.preventDefault();
+
+    if (spaceshipY >= 0 && spaceshipY <= 504 && spaceshipX >= 0 && spaceshipX <= 634) {
         switch (event.code) {
             case 'ArrowUp':
                 spaceshipY -= 10
@@ -129,14 +131,14 @@ function kDown(event) {
     if (spaceshipX < 0)
         spaceshipX = 0
 
-    if (spaceshipX > 304)
-        spaceshipX = 304
+    if (spaceshipX > 634)
+        spaceshipX = 634
 
     if (spaceshipY < 0)
         spaceshipY = 0
 
-    if (spaceshipY > 604)
-        spaceshipY = 604
+    if (spaceshipY > 504)
+        spaceshipY = 504
 }
 
 function kUp(event) {
@@ -206,7 +208,7 @@ function main() {
         requestAnimationFrame(main)
     }
     else
-        ctx.drawImage(gameOverImage, 50, 100, 300, 300)
+        ctx.drawImage(gameOverImage, 215, 100, 300, 300)
 }
 
 // 방향키를 누르면
