9d4bd8bc21bb9ed6a10228270294e185952dd471
hoyoung <skghdud024@naver.com>
2024-12-05 12:34:58 +0900

    메인페이지이미지보이게수정

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,13 +4,16 @@ module.exports = (app) => {
 
     const gameRouter = require("../routers/game/game_router")
     app.use("/game", gameRouter)
-    
+
     const boardRouter = require("../routers/board/board_router")
-    app.use("/board", boardRouter )
+    app.use("/board", boardRouter)
 
     const router = require("express").Router();
     router.get("/", (req, res) => {
-        res.render("main",{id : req.session.uid})
+        res.render("main_cover", { id: req.session.uid })
+    })
+    router.get("/main", (req, res) => {
+        res.render("main")
     })
 
     return router;
