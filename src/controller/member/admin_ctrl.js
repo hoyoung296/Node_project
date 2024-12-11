2624e973546116ccb07a69aa355d2297c07a3018
seongho <412jsh@naver.com>
2024-12-11 14:32:41 +0900

    관리자 회원삭제

---
+++ src/controller/member/admin_ctrl.js
@@ -0,0 +1,20 @@
+const ser = require("../../service/member/admin_service")
+const process = {
+    memberlist: async (req, res) => {
+        const data = await ser.process.ser_memberlist()
+        res.render("admin/memberlist", { data: data })
+    },
+    memberdelete: async (req, res) => {
+        const uid = req.params.uid;  // URL 경로에서 uid를 가져옴
+        console.log("Member ID to delete: ", uid);
+        try {
+            const msg = await ser.process.ser_memberdel(uid);
+            res.json({ success: true, message: msg });
+        } catch (err) {
+            console.log("Error in deleting member: ", err);
+            res.status(500).json({ success: false, error: "회원 삭제 실패" });
+        }
+    },
+}
+
+module.exports = { process }
---
+++ src/routers/member/admin_router.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const adminCtrl = require("../../controller/member/admin_ctrl");
+
+router.get("/memberlist",adminCtrl.process.memberlist);
+// 경로 파라미터를 사용하는 방법
+router.delete("/member_del/:uid", adminCtrl.process.memberdelete);
+
+
+
+module.exports = router;
