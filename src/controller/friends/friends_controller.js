d641d4245d48b09a2204286289318524b3af201a
hoyoung <skghdud024@naver.com>
2024-12-12 10:26:23 +0900

    친구추가오류해결

--- src/controller/friends/friends_controller.js
+++ src/controller/friends/friends_controller.js
@@ -5,12 +5,12 @@ const views = {
     list: async (req, res) => {
         const thema = await mctrl.userThema(req.session) //사용자 테마 설정
         await ser.pageRead.check(req.session.uid)
-        let data = await ser.pageRead.list(req.query.start,req.session.uid)
-        res.render("friends/list", { result: data.result, page: data.page, start: data.start, name: req.session.uid , thema })
+        let data = await ser.pageRead.list(req.query.start, req.session.uid)
+        res.render("friends/list", { result: data.result, page: data.page, start: data.start, name: req.session.uid, thema })
     },
     alram: async (req, res) => {
         const thema = await mctrl.userThema(req.session) //사용자 테마 설정
-        let data = await ser.pageRead.alram(req.query.start)
+        let data = await ser.pageRead.alram(req.query.start, req.session.uid)
         res.render("friends/alramList", { result: data.result, page: data.page, start: data.start, name: req.session.uid, thema })
     },
     view: async (req, res) => {
