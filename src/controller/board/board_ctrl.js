06ed11b95e900210f8e66f27295f4d1a4e09b87e
seongho <412jsh@naver.com>
2024-12-16 17:06:32 +0900

    관리자 회원삭제 페이징처리

--- src/controller/board/board_ctrl.js
+++ src/controller/board/board_ctrl.js
@@ -4,45 +4,32 @@ const path = require("path")
 const ejs = require('ejs');
 const mctrl = require("../controller") //thema설정하려고 추가
 
-
 const views = {//isLoggedIn: isLoggedIn 로그인 안하면 글쓰기 항목이 안보이는 코드 list.ejs에도 있음 
 
-    // Controller (views.main)
-    main: async (req, res) => {
-        console.log("req.query.start : ", req.query.start);
-
-        try {
-            const boardList = await ser.boardRead.list(req.query.start);
-            const isLoggedIn = req.session.user ? true : false;
-
-            const data = {
-                list: boardList.list,
-                start: boardList.start,
-                page: boardList.page,
-                isLoggedIn
-            };
-
-            // EJS 렌더링하여 HTML을 반환
-            ejs.renderFile(path.join(__dirname, '../../views/board/main_list.ejs'), data, (err, str) => {
-                if (err) {
-                    console.log(err);
-                    return res.status(500).send('Error rendering EJS');
-                }
-                res.send(str);  // HTML 텍스트로 반환
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(500).send('서버 오류');
-        }
-    },
-    topic: async (req, res) => {
-        const menu = req.query.menu;
-        console.log("menu :", menu)
-        const boardList = await ser.boardRead.list2(req.query.start, menu);
+    main : async (req, res) => {
+        const thema = await mctrl.userThema(req.session) //사용자 테마 설정
+        console.log(" req.query.start : ", req.query.start)
+        const boardList = await ser.boardRead.list( req.query.start );
         const isLoggedIn = req.session.user ? true : false;
-        const data = { list: boardList.list, start: boardList.start, page: boardList.page, isLoggedIn }
+        console.log("data : ",boardList.list)
+        const data = { list : boardList.list, start : boardList.start, page : boardList.page, isLoggedIn,thema }
         ejs.renderFile(path.join(__dirname, '../../views/board/main_list.ejs'), data, (err, str) => {
-            if (err) {
+            if(err){
+                console.log(err)
+                return res.status(500).send('Error rendering EJS')
+            }
+            res.send(str)
+        })
+        // res.json({ list : data.list, start : data.start, page : data.page, isLoggedIn })
+    }, 
+    topic : async (req, res) => {
+        const menu = req.query.menu;
+        console.log("menu :",menu)
+        const boardList = await ser.boardRead.list2( req.query.start,menu );
+        const isLoggedIn = req.session.user ? true : false;
+        const data = { list : boardList.list, start : boardList.start, page : boardList.page, isLoggedIn }
+        ejs.renderFile(path.join(__dirname, '../../views/board/main_list.ejs'), data, (err, str) => {
+            if(err){
                 console.log(err)
                 return res.status(500).send('Error rendering EJS')
             }
@@ -50,75 +37,75 @@ const views = {//isLoggedIn: isLoggedIn 로그인 안하면 글쓰기 항목이 
         })
         // res.json({ list : data.list, start : data.start, page : data.page, isLoggedIn })
     },
-    category: async (req, res) => {
+    category : async(req, res) => {
         const thema = await mctrl.userThema(req.session) //사용자 테마 설정
         const topic = req.params.category
         const menu = req.query.menu
-        res.render("board/categori", { thema, topic, menu })
+        res.render("board/categori", {thema,topic,menu})
     },
 
-    list: async (req, res) => {
+    list : async ( req, res ) => {
         const thema = await mctrl.userThema(req.session) //사용자 테마 설정
-        const data = await ser.boardRead.list(req.query.start);
+        const data = await ser.boardRead.list( req.query.start );
         const isLoggedIn = req.session.user ? true : false;
-        res.render("board/list", { list: data.list, start: data.start, page: data.page, isLoggedIn: isLoggedIn, thema })
+        res.render( "board/list", {list : data.list, start : data.start, page : data.page, isLoggedIn: isLoggedIn, thema })
     },
-    writeForm: async (req, res) => {
+    writeForm : async (req, res) => {
         const thema = await mctrl.userThema(req.session)
         console.log(req.session)
-        const msg = serCom.sessionCheck(req.session);
-        if (msg != 0) {
-            return res.send(msg)
+        const msg = serCom.sessionCheck( req.session );
+        if( msg != 0 ){
+            return res.send( msg )
         }
-        res.render("board/write_form", { username: req.session.name, thema })
+        res.render("board/write_form", {username : req.session.name, thema})
     },
-    data: async (req, res) => {
+    data : async ( req, res) => {
 
-        const data = await ser.boardRead.data(req.params.num);
+        const data = await ser.boardRead.data( req.params.num );
 
         const thema = await mctrl.userThema(req.session)
-        console.log("data : ", data)
+        console.log("data : ",data)
         const username = req.session.username;
-        res.render("board/data", { data, username, thema });
+        res.render("board/data", { data , username, thema } );
     },
-    modifyForm: async (req, res) => {
+    modifyForm : async ( req, res ) => {
         const thema = await mctrl.userThema(req.session)
-        const data = await ser.boardRead.data(req.params.writeNo);
-        res.render("board/modify_form", { data, thema });
+        const data = await ser.boardRead.data( req.params.writeNo );
+        res.render("board/modify_form", {data, thema});
     }
 }
 const process = {
-    write: async (req, res) => {
+    write : async ( req, res ) => {
         const msg = await ser.boardInsert.write(
-            req.body, req.file, req.fileValidation, req.session.uid, req.session.name
+            req.body, req.file, req.fileValidation, req.session.uid,req.session.name
         );
-        res.send(msg)
+        res.send( msg )
     },
     delete : ( req, res ) => {
-        file_process.delete( req.params.imgName );
+        file_process.delete( req.params.writeNo );
         ser.boardUpdate.delete( req.params.writeNo );
         res.redirect("/board/list");
     },
-    modify: async (req, res) => {
+    modify : async ( req, res) => {
         const deleteFile = req.body.change_file;
-        const message = await ser.boardUpdate.modify(req.body, req.file);
-        if (req.file !== undefined && message.result === 1) {
-            file_process.delete(deleteFile);
+        const message = await ser.boardUpdate.modify( req. body, req.file );
+        if( req.file !== undefined && message.result ===1 ){
+            file_process.delete( deleteFile );
         }
-        res.send(message.msg);
+        res.send(message.msg );
     }
 }
 const fs = require("fs");
 const file_process = {
     download : ( req, res ) => {
-        console.log("req.params.imgName : ", req.params.imgName)
+        console.log("req.params.imgName : ",req.params.imgName)
         const filePath = `./upload_file/${req.params.imgName}`;
-        res.download(filePath)
+        res.download( filePath )
     },
-    delete: (imgName) => {
-        if (imgName !== 'nan') {
-            fs.unlinkSync(`./upload_file/${imgName}`);
+    delete : ( imgName ) => {
+        if( imgName !== 'nan'){
+            fs.unlinkSync(`./upload_file/${ imgName }`);
         }
-    }
+    } 
 }
 module.exports = { file_process, process, views }
